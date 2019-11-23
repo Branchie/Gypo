dad57b91ab37c696a9f475b0e506c6d140a239da
Branchie <michaelsemps@hotmail.co.uk>
2019-11-24 00:36:33 +0100

    IList<T> printing extension functions.

--- Scripts/Classes/Static/Extensions.IList.cs
+++ Scripts/Classes/Static/Extensions.IList.cs
@@ -1,6 +1,7 @@
 ﻿namespace Gypo
 {
 	using System.Collections.Generic;
+	using System.Text;
 	using UnityEngine;
 
 	public static partial class Extensions
@@ -8,6 +9,40 @@
 		public static T GetRandom<T>(this IList<T> l)
 		{
 			return l[Random.Range(0, l.Count)];
+		}
+
+		public static string ToCSV<T>(this IList<T> l) => l.ToString(',');
+		public static string ToSpacedCSV<T>(this IList<T> l) => l.ToString(", ");
+		public static string ToString<T>(this IList<T> l, char seperator)
+		{
+			if (l.Count == 0)
+				return "";
+
+			StringBuilder builder = new StringBuilder(l[0].ToString());
+
+			for (int i = 1; i < l.Count; i++)
+			{
+				builder.Append(seperator);
+				builder.Append(l[i].ToString());
+			}
+
+			return builder.ToString();
+		}
+
+		public static string ToString<T>(this IList<T> l, string seperator)
+		{
+			if (l.Count == 0)
+				return "";
+
+			StringBuilder builder = new StringBuilder(l[0].ToString());
+
+			for (int i = 1; i < l.Count; i++)
+			{
+				builder.Append(seperator);
+				builder.Append(l[i].ToString());
+			}
+
+			return builder.ToString();
 		}
 	}
 }
