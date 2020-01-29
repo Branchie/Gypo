21cafd99f3ef8985fceff4818c6182f5ef36a8c2
Branchie <michaelsemps@hotmail.co.uk>
2020-01-29 22:56:14 +0100

    Asset Reserializer editor tool.

---
+++ Scripts/Editor/Reserializer.cs
@@ -0,0 +1,13 @@
+﻿namespace Gypo.Editor
+{
+	using UnityEditor;
+
+	public static class Reserializer
+	{
+		[MenuItem("Assets/Reserialize All")]
+		public static void ReserializeAll()
+		{
+			AssetDatabase.ForceReserializeAssets();
+		}
+	}
+}
