2d3e664467000d5fcc4164126d9eea489947c99b
Branchie <michaelsemps@hotmail.co.uk>
2020-01-31 01:33:46 +0100

    CSV extensions now support custom string providers.

--- Scripts/Classes/Static/Extensions.IList.cs
+++ Scripts/Classes/Static/Extensions.IList.cs
@@ -1,45 +1,50 @@
 ﻿namespace Gypo
 {
+	using System;
 	using System.Collections.Generic;
 	using System.Text;
-	using UnityEngine;
 
 	public static partial class Extensions
 	{
 		public static T GetRandom<T>(this IList<T> l)
 		{
-			return l[Random.Range(0, l.Count)];
+			return l[UnityEngine.Random.Range(0, l.Count)];
 		}
 
-		public static string ToCSV<T>(this IList<T> l) => l.ToString(',');
-		public static string ToSpacedCSV<T>(this IList<T> l) => l.ToString(", ");
-		public static string ToString<T>(this IList<T> l, char seperator)
+		public static string ToCSV<T>(this IList<T> l)								=> l.ToString(',');
+		public static string ToCSV<T>(this IList<T> l, Func<T, string> str)			=> l.ToString(str, ',');
+		public static string ToSpacedCSV<T>(this IList<T> l)						=> l.ToString(", ");
+		public static string ToSpacedCSV<T>(this IList<T> l, Func<T, string> str)	=> l.ToString(str, ", ");
+		public static string ToString<T>(this IList<T> l, char seperator)			=> l.ToString(i => i.ToString(), seperator);
+		public static string ToString<T>(this IList<T> l, string seperator)			=> l.ToString(i => i.ToString(), seperator);
+
+		public static string ToString<T>(this IList<T> l, Func<T, string> str, char seperator)
 		{
 			if (l.Count == 0)
 				return "";
 
-			StringBuilder builder = new StringBuilder(l[0].ToString());
+			StringBuilder builder = new StringBuilder(str(l[0]));
 
 			for (int i = 1; i < l.Count; i++)
 			{
 				builder.Append(seperator);
-				builder.Append(l[i].ToString());
+				builder.Append(str(l[i]));
 			}
 
 			return builder.ToString();
 		}
 
-		public static string ToString<T>(this IList<T> l, string seperator)
+		public static string ToString<T>(this IList<T> l, Func<T, string> str, string seperator)
 		{
 			if (l.Count == 0)
 				return "";
 
-			StringBuilder builder = new StringBuilder(l[0].ToString());
+			StringBuilder builder = new StringBuilder(str(l[0]));
 
 			for (int i = 1; i < l.Count; i++)
 			{
 				builder.Append(seperator);
-				builder.Append(l[i].ToString());
+				builder.Append(str(l[i]));
 			}
 
 			return builder.ToString();
