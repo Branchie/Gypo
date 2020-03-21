63482046a5e370862bdbfd9f33345616ef7900a0
Branchie <michaelsemps@hotmail.co.uk>
2020-03-21 18:10:41 +0100

    Transform extensions for setting vectors.

---
+++ Scripts/Classes/Static/Extensions.Transform.cs
@@ -0,0 +1,55 @@
+﻿namespace Gypo
+{
+	using UnityEngine;
+
+	public static partial class Extensions
+	{
+		public static void MulLocalScale_X(this Transform t, float x)
+		{
+			Vector3 localScale = t.localScale;
+			localScale.x *= x;
+
+			t.localScale = localScale;
+		}
+
+		public static void MulLocalScale_Y(this Transform t, float y)
+		{
+			Vector3 localScale = t.localScale;
+			localScale.y *= y;
+
+			t.localScale = localScale;
+		}
+
+		public static void MulLocalScale_Z(this Transform t, float z)
+		{
+			Vector3 localScale = t.localScale;
+			localScale.z *= z;
+
+			t.localScale = localScale;
+		}
+
+		public static void SetLocalScale_X(this Transform t, float x)
+		{
+			Vector3 localScale = t.localScale;
+			localScale.x = x;
+
+			t.localScale = localScale;
+		}
+
+		public static void SetLocalScale_Y(this Transform t, float y)
+		{
+			Vector3 localScale = t.localScale;
+			localScale.y = y;
+
+			t.localScale = localScale;
+		}
+
+		public static void SetLocalScale_Z(this Transform t, float z)
+		{
+			Vector3 localScale = t.localScale;
+			localScale.z = z;
+
+			t.localScale = localScale;
+		}
+	}
+}
