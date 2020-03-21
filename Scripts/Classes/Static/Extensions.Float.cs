64c9217bf72cb89c7b2ef3a897063692baa8673c
Branchie <michaelsemps@hotmail.co.uk>
2020-03-21 18:10:31 +0100

    Float math extensions: Abs, Sign, and SignToInt.

--- Scripts/Classes/Static/Extensions.Float.cs
+++ Scripts/Classes/Static/Extensions.Float.cs
@@ -4,6 +4,11 @@
 
 	public static partial class Extensions
 	{
+		public static float Abs(this float f)
+		{
+			return Mathf.Abs(f);
+		}
+
 		public static bool Approximately(this float a, float b, float fudge)
 		{
 			return Mathf.Abs(a - b) < fudge;
@@ -22,6 +27,16 @@
 		public static float Round(this float f, float precision)
 		{
 			return Mathf.Round(f / precision) * precision;
+		}
+
+		public static float Sign(this float f)
+		{
+			return f > 0.0f ? 1.0f : -1.0f;
+		}
+
+		public static int SignToInt(this float f)
+		{
+			return f > 0 ? 1 : -1;
 		}
 	}
 }
