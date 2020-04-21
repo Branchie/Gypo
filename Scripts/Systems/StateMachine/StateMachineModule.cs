bced514a408f51e3a0684ffa53823bee18686531
Branchie <michaelsemps@hotmail.co.uk>
2020-04-21 23:00:06 +0200

    StateMachineModule as a way for StateMachines to make use of Monobehaviour callbacks.

---
+++ Scripts/Systems/StateMachine/StateMachineModule.cs
@@ -0,0 +1,50 @@
+﻿namespace Gypo.StateMachine
+{
+	using Triggers;
+	using UnityEngine;
+
+	public class StateMachineModule : MonoBehaviour
+	{
+		public StateMachine stateMachine = new StateMachine();
+
+		protected virtual void FixedUpdate()
+		{
+			stateMachine.Trigger<OnFixedUpdate>();
+		}
+
+		protected virtual void Update()
+		{
+			stateMachine.Trigger<OnUpdate>();
+		}
+
+		protected virtual void OnCollisionEnter2D(Collision2D collision)
+		{
+			stateMachine.Trigger(new OnCollisionEnter2D(collision));
+		}
+
+		protected virtual void OnCollisionStay2D(Collision2D collision)
+		{
+			stateMachine.Trigger(new OnCollisionStay2D(collision));
+		}
+
+		protected virtual void OnCollisionExit2D(Collision2D collision)
+		{
+			stateMachine.Trigger(new OnCollisionExit2D(collision));
+		}
+
+		protected virtual void OnTriggerEnter2D(Collider2D other)
+		{
+			stateMachine.Trigger(new OnTriggerEnter2D(other));
+		}
+
+		protected virtual void OnTriggerStay2D(Collider2D other)
+		{
+			stateMachine.Trigger(new OnTriggerStay2D(other));
+		}
+
+		protected virtual void OnTriggerExit2D(Collider2D other)
+		{
+			stateMachine.Trigger(new OnTriggerExit2D(other));
+		}
+	}
+}
